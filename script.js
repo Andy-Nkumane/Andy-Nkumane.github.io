7183d412da7782d21c60202772675df7cb1c3fa9
Andy Nkumane <Andy.Nkumane@bbd.co.za>
2025-02-03 19:10:38 +0200

    cleaned up code + added comments

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ const API_KEY = "6c4366d9704b4d6bbd1111224250302";
 // get user's location
 function getLocation() { 
     if (navigator.geolocation) { 
-        navigator.geolocation.getCurrentPosition(showPosition, showError); 
+        navigator.geolocation.getCurrentPosition(weather, showError); 
     } else { 
         console.error("Geolocation is not supported by this browser."); 
     } 
-} 
- 
-function showPosition(position) { 
-    const latitude = position.coords.latitude; 
-    const longitude = position.coords.longitude; 
-    console.log("Latitude: " + latitude + ", Longitude: " + longitude); 
-
-    weather(latitude, longitude);
 } 
  
 function showError(error) { 
@@ -35,13 +27,16 @@ function showError(error) {
     } 
 } 
 
-function weather(latitude, longitude) {
-    console.log(`------- weather ${latitude} --------`);
-    console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+function weather(position) {
+    // get user's location
+    const latitude = position.coords.latitude; 
+    const longitude = position.coords.longitude; 
 
-
-
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude}, ${longitude}`)
+    // calling api using GET to retrieve data from the provided location
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude}, ${longitude}`,{
+        "method": "GET",
+        "headers": {}
+    })
     .then(response => {
         if (response.ok) {
             return response.json(); // Parse the response data as JSON
@@ -53,7 +48,6 @@ function weather(latitude, longitude) {
         // Process the response data here
         console.log(data); 
         const weatherSection = document.getElementById("weather");
-        const weatherDetailsDiv = document.getElementById("weather-details");
         weatherSection.innerHTML = `<p>Country: ${data.location.country} 
         <br> 
         City/Town: ${data.location.name}
@@ -66,10 +60,6 @@ function weather(latitude, longitude) {
         <br> 
         Last updated: ${data.current.last_updated}</p>
         <img src=https:${data.current.condition.icon} alt=${data.current.condition.text}>`
-        // weatherSection.innerHTML = `<ul>Country: ${data.current.condition.icon}</ul>`
-        console.log(data.current.condition.icon);
-        // console.log(console.log(data.current.condition.icon.slice(2)));
-        console.log(data.current.condition.text);
     })
     .catch(error => {
         // Handle any errors here
@@ -77,6 +67,7 @@ function weather(latitude, longitude) {
     });
 }
 
+// when page loads this will run
 window.onload = function() { 
     getLocation(); 
 }; 
